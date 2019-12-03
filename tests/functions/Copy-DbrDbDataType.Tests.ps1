f438ce6d4e5c8183a4570dfb82c115606724c704
sqlstad <sanderstad@gmail.com>
2019-12-03 14:10:50 +0100

    Renamed parameter

--- tests/functions/Copy-DbrDbDataType.Tests.ps1
+++ tests/functions/Copy-DbrDbDataType.Tests.ps1
@@ -4,7 +4,7 @@ $CommandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
 Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
     Context "Validate parameters" {
         [object[]]$params = (Get-Command $CommandName).Parameters.Keys | Where-Object { $_ -notin ('whatif', 'confirm') }
-        $knownParameters = 'SourceSqlInstance', 'SourceSqlCredential', 'DestinationSqlInstance', 'DestinationSqlCredential', 'Database', 'Schema', 'Uddt', 'EnableException'
+        $knownParameters = 'SourceSqlInstance', 'SourceSqlCredential', 'DestinationSqlInstance', 'DestinationSqlCredential', 'Database', 'Schema', 'DataType', 'EnableException'
         $knownParameters += [System.Management.Automation.PSCmdlet]::CommonParameters
         It "Should only contain our specific parameters" {
             (@(Compare-Object -ReferenceObject ($knownParameters | Where-Object { $_ }) -DifferenceObject $params).Count ) | Should Be 0
