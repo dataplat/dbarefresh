e45c34e8cf866f69ad4f611b5f42bc921fa305ce
sqlstad <sanderstad@gmail.com>
2019-12-19 13:27:14 +0100

    Added extra tests for command

--- tests/functions/internal/ConvertFrom-DbrConfig.Tests.ps1
+++ tests/functions/internal/ConvertFrom-DbrConfig.Tests.ps1
@@ -18,10 +18,37 @@ Describe "$CommandName Integration Tests" -Tag 'IntegrationTests' {
     }
 
     Context "Test for errors" {
-
         It "Should generate FilePath error" {
             $failingfilepath = ($jsonFilePathFail.Substring(0, $jsonFilePathFail.Length - 1))
             { ConvertFrom-DbrConfig -FilePath $failingfilepath -EnableException } | Should -Throw "Could not find configuration file"
+        }
+    }
+
+    Context "Test command" {
+
+        $result = ConvertFrom-DbrConfig -FilePath $jsonFilePathSuccess
+
+        It "Should return objects" {
+            $result | Should -Not -Be $null
+        }
+
+        It "Should have all the databases" {
+            $result.databases.count | Should -Be 2
+        }
+
+        It "Should return the correct databases" {
+            $result.databases[1].sourceinstance | Should -Be "localhost"
+            $result.databases[1].sourcedatabase | Should -BeExactly  "DB2"
+            $result.databases[1].destinationinstance | Should -Be "localhost"
+            $result.databases[1].destinationdatabase | Should -BeExactly  "DB2"
+        }
+
+        It "Should have the correct amount of tables" {
+            $result.databases[0].tables.count | Should -Be 2
+        }
+
+        It "Should have correct amount of columns" {
+            $result.databases[0].tables[0].columns.count | Should -Be 3
         }
     }
 
