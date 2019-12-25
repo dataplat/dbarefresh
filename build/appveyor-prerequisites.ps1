7554b95b4ee14c45253e4c3fd3f6f571820f9028
sqlstad <sanderstad@gmail.com>
2019-12-25 13:36:53 +0100

    Changed dir check to include version Changed min version to max version

--- build/appveyor-prerequisites.ps1
+++ build/appveyor-prerequisites.ps1
@@ -2,23 +2,23 @@ Add-AppveyorTest -Name "appveyor.prerequisites" -Framework NUnit -FileName "appv
 $sw = [system.diagnostics.stopwatch]::startNew()
 
 #Write-Host -Object "appveyor.prep: Install psframework" -Level Important
-if (-not(Test-Path 'C:\Program Files\WindowsPowerShell\Modules\psframework')) {
-    Install-Module -Name psframework -Force -SkipPublisherCheck -MinimumVersion 1.0 | Out-Null
+if (-not(Test-Path 'C:\Program Files\WindowsPowerShell\Modules\psframework\1.1.59')) {
+    Install-Module -Name psframework -Force -SkipPublisherCheck -MaximumVersion 1.1.59 | Out-Null
 }
 
 Write-PSFMessage -Message "appveyor.prep: Install Pester" -Level Important
-if (-not(Test-Path 'C:\Program Files\WindowsPowerShell\Modules\Pester')) {
-    Install-Module -Name Pester -Force -SkipPublisherCheck -MinimumVersion 4.9 | Out-Null
+if (-not(Test-Path 'C:\Program Files\WindowsPowerShell\Modules\Pester\4.9.0')) {
+    Install-Module -Name Pester -Force -SkipPublisherCheck -MaximumVersion 4.9.0 | Out-Null
 }
 
 Write-PSFMessage -Message "appveyor.prep: Install dbatools" -Level Important
-if (-not(Test-Path 'C:\Program Files\WindowsPowerShell\Modules\dbatools')) {
-    Install-Module -Name dbatools -Force -SkipPublisherCheck -MinimumVersion 1.0 | Out-Null
+if (-not(Test-Path 'C:\Program Files\WindowsPowerShell\Modules\dbatools\1.0.77')) {
+    Install-Module -Name dbatools -Force -SkipPublisherCheck -MaximumVersion 1.0.77 | Out-Null
 }
 
 Write-PSFMessage -Message "appveyor.prep: Install psscriptanalyzer" -Level Important
-if (-not(Test-Path 'C:\Program Files\WindowsPowerShell\Modules\psscriptanalyzer')) {
-    Install-Module -Name psscriptanalyzer -Force -SkipPublisherCheck -MinimumVersion 1.0 | Out-Null
+if (-not(Test-Path 'C:\Program Files\WindowsPowerShell\Modules\psscriptanalyzer\1.18.3')) {
+    Install-Module -Name psscriptanalyzer -Force -SkipPublisherCheck -MaximumVersion 1.18.3 | Out-Null
 }
 
 $pesterVersion = (Get-Module -Name Pester).Version
