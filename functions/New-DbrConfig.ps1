8ada782c3a267cfdbfada9d3d08223dcc3fe14c3
sqlstad <sanderstad@gmail.com>
2020-01-03 22:52:51 +0100

    Initial commit

---
+++ functions/New-DbrConfig.ps1
@@ -0,0 +1,103 @@
+function New-DbrConfig {
+
+    param(
+        [parameter(Mandatory)]
+        [DbaInstanceParameter]$SqlInstance,
+        [PSCredential]$SqlCredential,
+        [parameter(Mandatory)]
+        [string[]]$Database,
+        [string]$OutFilePath,
+        [string[]]$Schema,
+        [string[]]$Table,
+        [switch]$EnableException
+    )
+
+    begin {
+        # Connect to the source instance
+        $server = Connect-DbaInstance -SqlInstance $SqlInstance -SqlCredential $SqlCredential
+
+        $databases = $server.Databases | Where-Object Name -in $Database
+
+        if ((Test-Path -Path $OutFilePath)) {
+            if ((Get-Item $OutFilePath) -isnot [System.IO.FileInfo]) {
+                Stop-PSFFunction -Message "OutFilePath is a directory. Please enter a path for a file"
+            }
+        }
+        else {
+            try {
+                $null = New-Item -Path $OutFilePath -ItemType File
+            }
+            catch {
+                Stop-PSFFunction -Message "Could not create output file" -Target $OutFilePath
+            }
+        }
+    }
+
+    process {
+        if (Test-PSFFunctionInterrupt) { return }
+
+        $result = @()
+
+        $databaseObjectArray = @()
+
+        foreach ($db in $databases) {
+            Write-PSFMessage -Message "Retrieving objects from database $($db.Name)" -Level Verbose
+
+            $tables = $db.Tables
+
+            if ($Schema) {
+                [array]$tables = $tables | Where-Object Schema -in $Schema
+            }
+
+            if ($Table) {
+                [array]$tables = $tables | Where-Object Name -in $Table
+            }
+
+            if ($tables.Count -lt 1) {
+                Stop-PSFFunction -Message "No tables to process for database $db"
+            }
+            else {
+                $tableObjectArray = @()
+
+                Write-PSFMessage -Message "Retrieving tables from database $($db.Name)" -Level Verbose
+
+                foreach ($tableObject in $tables) {
+                    $columns = $tableObject.Columns
+
+                    $columnObjectArray = @()
+
+                    Write-PSFMessage -Message "Retrieving columns from table [$($tableObject.Schema)].[$($tableObject.Name)]" -Level Verbose
+
+                    foreach ($columnObject in $columns) {
+                        $columnObjectArray += [PSCustomObject]@{
+                            name     = $columnObject.Name
+                            datatype = $columnObject.datatype
+                            filter   = $($null)
+                        }
+                    }
+
+                    $tableObjectArray += [PSCustomObject]@{
+                        fullname = "$($tableObject.Schema).$($tableObject.Name)"
+                        schema   = $tableObject.Schema
+                        name     = $tableObject.Name
+                        columns  = $columnObjectArray
+                        query    = $null
+                    }
+                }
+
+                $databaseObjectArray += [PSCustomObject]@{
+                    sourceinstance      = $server.DomainInstanceName
+                    destinationinstance = $server.DomainInstanceName
+                    database            = $db.Name
+                    tables              = $tableObjectArray
+                }
+            }
+
+            $result += [PSCustomObject]@{
+                databases = $databaseObjectArray
+            }
+        }
+
+        $databaseObjectArray | ConvertTo-Json | Set-Content -Path $OutFilePath
+    }
+}
---
+++ tests/functions/New-DbrConfig.Tests.ps1
@@ -0,0 +1,14 @@
+$CommandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
+. "$PSScriptRoot\constants.ps1"
+
+Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
+    Context "Validate parameters" {
+        It "Should have the correct parameters" {
+            Get-Command $CommandName | Should -HaveParameter SqlInstance -Type DbaInstanceParameter -Mandatory
+            Get-Command $CommandName | Should -HaveParameter SqlCredential -Type PSCredential
+            Get-Command $CommandName | Should -HaveParameter Database -Type string[] -Mandatory
+            Get-Command $CommandName | Should -HaveParameter Schema -Type string[]
+            Get-Command $CommandName | Should -HaveParameter Table -Type string[]
+        }
+    }
+}
